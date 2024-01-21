7f628829844f4ae713488170aa82e36233bf67ad
programmer-SON <sjw3957@gmail.com>
2024-01-22 00:12:01 +0900

    섹션1 - Outlet을 이용한 서브메뉴

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -6,9 +6,15 @@ const BasicMenu = () => {
         <nav id='navbar' className =" flex bg-blue-300">
             <div className ="w-4/5 bg-gray-500" >
                 <ul className ="flex p-4 text-white font-bold">
-                    <li className ="pr-6 text-2xl"><Link to={'/'}>Main</Link></li>
-                    <li className ="pr-6 text-2xl"><Link to={'/about'}>About</Link></li>
-                    <li className ="pr-6 text-2xl"><Link to={'/todo/'}>Todo</Link></li>
+                    <li className ="pr-6 text-2xl">
+                        <Link to={'/'}>Main</Link>
+                    </li>
+                    <li className ="pr-6 text-2xl">
+                        <Link to={'/about'}>About</Link>
+                    </li>
+                    <li className ="pr-6 text-2xl">
+                        <Link to={'/todo/'}>Todo</Link>
+                    </li>
                 </ul>
             </div>
             <div className ="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
--- src/router/root.js
+++ src/router/root.js
@@ -4,7 +4,8 @@ const {createBrowserRouter}=require("react-router-dom");
 const Loading = <div className={'bg-red-700'}>Loading...</div>
 const Main = lazy(()=>import("../pages/MainPage"))
 const About = lazy(()=>import("../pages/AboutPage"))
-
+const TodoIndex = lazy(()=>import("../pages/todo/indexPage"))
+const TodoList = lazy(()=>import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
 
@@ -15,6 +16,16 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: [
+            {
+                path: 'list',
+                element: <Suspense fallback={Loading}><TodoList/></Suspense>
+            }
+        ]
     }
 ])
 
