846342d5cc7e25d85b270369a2ce8a7423620e17
programmer-SON <sjw3957@gmail.com>
2024-03-25 20:37:35 +0900

    섹션5 - 상품관련 React-Router 설정

---
+++ src/pages/products/indexPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function IndexPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default IndexPage;
--- src/router/root.js
+++ src/router/root.js
@@ -1,12 +1,14 @@
 import {Suspense, lazy} from "react";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
+
 const {createBrowserRouter}=require("react-router-dom");
 
 const Loading = <div className={'bg-red-700'}>Loading...</div>
 const Main = lazy(()=>import("../pages/MainPage"))
 const About = lazy(()=>import("../pages/AboutPage"))
 const TodoIndex = lazy(()=>import("../pages/todo/indexPage"))
-const TodoList = lazy(()=>import("../pages/todo/ListPage"))
+const productsIndex = lazy(() => import("../pages/products/indexPage"))
 
 const root = createBrowserRouter([
 
@@ -22,6 +24,11 @@ const root = createBrowserRouter([
         path: "todo",
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children: todoRouter()
+    },
+    {
+        path: "products",
+        element: <Suspense fallback={Loading}><productsIndex/></Suspense>,
+        children: productsRouter()
     }
 ])
 
